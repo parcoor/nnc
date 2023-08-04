3904d29869512b44d7633dde1a4d02d0b95a15f6
Manuel Capel <manuel.capel82@gmail.com>
2023-08-04 17:42:13 +0200

    load_network() value reading completed

--- persistence.c
+++ persistence.c
@@ -15,6 +15,15 @@ int persist_network(network *nk, char *fp)
     return EXIT_SUCCESS;
 }
 
+bool is_figure(char c)
+{
+    return ((c == '0') || (c == '1') || (c == '2') || (c == '3') || (c == '4') || (c == '5') || (c == '6') || (c == '7') || (c == '8') || (c == '9'));
+}
+bool is_numeric(char c)
+{
+    return (is_figure(c) || (c == '-') || (c == '.'));
+}
+
 int load_network(char *fp)
 {
     FILE *f = fopen(fp, "r");
@@ -30,15 +39,17 @@ int load_network(char *fp)
     int cur_stage = 0; // current stage
     bool gonextline = false;
     bool is_firstword = true; // is first word in line?
-    char firstword[11];
-    char word[9];
+    char firstword[11];       // used to identify following numerical value(s)
     uint8_t ind_firstword = 0;
     bool read_int = false;
     bool read_array = false;
     bool read_word = false;
+    char current_ch[10] = {0}; // currently read value (integer, float or word)
+    uint8_t current_ind = 0;   // character index when reading word or number
+    uint8_t array_ind = 0;
     while ((c = fgetc(f)) != EOF)
     {
-        if (gonextline && (char)c != '\n')
+        if ((gonextline && (char)c != '\n') || (read_word && ((char)c == ' ' || (char)c == ':')))
             continue;
 
         else if ((char)c == '\n')
@@ -52,6 +63,22 @@ int load_network(char *fp)
             }
             if (neq > 0)
                 neq = 0;
+            if (read_int)
+            {
+                current_ch[current_ind] = '\0';
+                current_ind = 0;
+                read_int = false;
+                // TODO: attribute number to right value
+                printf("[DEBUG] load_network(): read int: %s\n", current_ch);
+            }
+            if (read_word)
+            {
+                current_ch[current_ind] = '\0';
+                current_ind = 0;
+                read_word = false;
+                // TODO: attribute word to right value
+                printf("[DEBUG] load_network(): read word: %s\n", current_ch);
+            }
             continue;
         }
 
@@ -83,12 +110,43 @@ int load_network(char *fp)
             ind_firstword = 0;
             printf("[DEBUG] load_network(): cur_stage=%i, firstword: %s\n", cur_stage, firstword);
             // TODO: handle depending on cur_stage and firstword
-            if (!strncpm(firstword, "Max", 4) || !strncpm(firstword, "Current", 8) || !strncpm(firstword, "Input", 5) || !strncpm(firstword, "Number", 7))
+            if (!strncmp(firstword, "Max", 4) || !strncmp(firstword, "Current", 8) || !strncmp(firstword, "Input", 5) || !strncmp(firstword, "Number", 7))
                 read_int = true;
-            else if (!strncpm(firstword, "weights", 9) || !strncpm(firstword, "biases", 7))
+            else if (!strncmp(firstword, "weights", 9) || !strncmp(firstword, "biases", 7))
                 read_array = true;
-            else if (!strncpm(firstword, "Activation", 11))
-                read_word = true
+            else if (!strncmp(firstword, "Activation", 11))
+                read_word = true;
+        }
+
+        else if ((read_int && is_figure((char)c)) || (read_array && is_numeric((char)c)))
+        {
+            current_ch[current_ind] = (char)c;
+            current_ind++;
+        }
+
+        else if (read_array && (char)c == ',')
+        {
+            current_ch[current_ind] = '\0';
+            current_ind = 0;
+            // TODO: attribute word to right value
+            printf("[DEBUG] load_network(): read array float: %s (index: %u)\n", current_ch, array_ind);
+            array_ind++;
+        }
+
+        else if (read_array && (char)c == ']')
+        {
+            current_ch[current_ind] = '\0';
+            current_ind = 0;
+            read_array = false;
+            // TODO: attribute word to right value
+            printf("[DEBUG] load_network(): read array float: %s (index: %u)\n", current_ch, array_ind);
+            array_ind = 0;
+        }
+
+        else if (read_word)
+        {
+            current_ch[current_ind] = (char)c;
+            current_ind++;
         }
     }
 
